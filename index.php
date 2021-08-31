74c6c79d8acfc40fb67d94e3bebb5b4ba76df6d0
brunobenchimol <40777698+brunobenchimol@users.noreply.github.com>
2021-08-31 17:08:59 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 
-echo 'I am running on: <b>' . gethostname() . '</b><br><br><br>'; 
+echo 'I am running on: <b>' . gethostname() . '</b><br><br>'; 
 
+echo '<A HREF="phpinfo.php">Check phpinfo()</A> <br><br>'
 
+echo '---------------- Outputing Server Vars and Headers ----------------';
+echo '<br><br>';
+  
 $headers =  getallheaders();
 foreach($headers as $key=>$val){
   echo $key . ': ' . $val . '<br>';

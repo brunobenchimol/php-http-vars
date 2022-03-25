7d947b648fe3aa7aade9258aba7ec230d8453695
brunobenchimol <40777698+brunobenchimol@users.noreply.github.com>
2022-03-25 19:33:52 -0300

    added reading version from composer.json

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
 echo 'I am running on: <b>' . gethostname() . '</b><br><br>'; 
+
+$content = file_get_contents('composer.json');
+$content = json_decode($content,true);
+echo 'Build Version: <b>' . $content['version']; . '</b><br><br>';
+
 echo 'Operational System Environment Variables<br>';
 echo 'TZ = ' . getenv("TZ") . '<br>';
 echo 'CUSTOM_VAR = ' . getenv("CUSTOM_VAR") . '<br>';
